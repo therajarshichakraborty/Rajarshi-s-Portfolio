74ebafcb931b229e4768eb6ccdfefd6c7d5502cd
Rajarshi Chakraborty <rajarshichakraborty2005@gmail.com>
2026-07-26 16:48:32 +0530

    fix: change framer-motion import to motion/react

--- src/components/gptCard/brandCard.tsx
+++ src/components/gptCard/brandCard.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { motion } from "motion/react";
 import { Button } from "../ui/button";
 import { LeetCode } from "../ui/svgs/leetcode";
 import Link from "next/link";
